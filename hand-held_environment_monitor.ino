317307641e664edd567d2526aa546a2c98864ca4
xpanhhu <babilun.123@gmail.com>
2015-10-10 14:25:44 +0800

    1. correct temperature transfer key error; 2. adjust OLED display.

--- hand-held_environment_monitor.ino
+++ hand-held_environment_monitor.ino
@@ -59,11 +59,13 @@ void loop() {
                         sensorValues[TEMPERATURE_VALUE_INDEX],
                         sensorValues[HUMIDITY_VALUE_INDEX]);
 
+  displaySampling();
+
   curlPostData(sensorValues[AIRQ_VALUE_INDEX], SENSOR_AIRQ_VALUE_INDEX);//to consider removing from web
   curlPostData(sensorValues[HCHO_VALUE_INDEX], SENSOR_HCHO_VALUE_INDEX);
   curlPostData(sensorValues[DUST_VALUE_INDEX], SENSOR_DUST_VALUE_INDEX);
   curlPostData(sensorValues[CH4_VALUE_INDEX], SENSOR_CH4_VALUE_INDEX);
-  curlPostData(sensorValues[TEMPERATURE_VALUE_INDEX], SENSOR_CH4_VALUE_INDEX);
+  curlPostData(sensorValues[TEMPERATURE_VALUE_INDEX], SENSOR_TEMPERATURE_VALUE_INDEX);
   curlPostData(sensorValues[HUMIDITY_VALUE_INDEX], SENSOR_HUMIDITY_VALUE_INDEX);
 
   delay(10000);
