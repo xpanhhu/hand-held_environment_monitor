e88caf1b6e41da636a11df18027efe9b57f41a86
feng-wei <feng0726@126.com>
2015-10-18 15:04:07 +0800

    update threshold value

--- hand-held_environment_monitor.h
+++ hand-held_environment_monitor.h
@@ -43,8 +43,8 @@
 #define DHT_TEMP_DATA_INDEX 4
 #define DHT_HUMIDITY_DATA_INDEX 5
 
-const float ALARM_THRESHOLD_MINS[] = { 0, 0, 0, 0, 15, 30};
-const float ALARM_THRESHOLD_MAXS[] = { 2000, 0.08, 1, 2000, 30, 60};
+const float ALARM_THRESHOLD_MINS[] = {0, 0, -1, 0, 15, 30};
+const float ALARM_THRESHOLD_MAXS[] = {2000, 0.08, 1, 2000, 30, 60};
 
 #endif
 
